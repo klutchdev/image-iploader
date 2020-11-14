9893d8eeccff4725180b52dc71b35cb5c13ea0f7
KlutchDEV <klutchdevelopment@gmail.com>
2020-11-14 13:34:45 -0500

    fix storage creds

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/storage";
 
 firebase.initializeApp({
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: "AIzaSyDLfKOXwWZVU-EHiVuNivMjbzZHA66VGpA",
+  authDomain: "image-uploader-a4258.firebaseapp.com",
+  databaseURL: "https://image-uploader-a4258.firebaseio.com",
+  projectId: "image-uploader-a4258",
+  storageBucket: "image-uploader-a4258.appspot.com",
+  messagingSenderId: "390530709910",
+  appId: "1:390530709910:web:df35caf724bb748c9ac5b7",
 });
 
 const storage = firebase.storage();
